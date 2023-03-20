3c6fcffb90a797a3cff4fd0b3a59ecbf45f8034b
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-03-20 19:56:31 +0900

    feat: 画像投稿画面への遷移メソッド作成

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import style from './Home.module.scss';
 
+import { PAGES_ROUTE } from '@/constants/common/route';
+
 const Home: FC = () => {
+  const navigate = useNavigate();
+
+  const handleClickRouterImage = () => {
+    navigate(PAGES_ROUTE.IMAGE);
+  };
+
   return (
     <div className={style['home-page']}>
-      <button className={style['action']}>画像を投稿する</button>
+      <button onClick={handleClickRouterImage} className={style['action']}>
+        画像を投稿する
+      </button>
     </div>
   );
 };
