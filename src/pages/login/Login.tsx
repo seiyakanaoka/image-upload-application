803fcded0852d4d8ed1dcbcd09503b0cc83627be
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-03-20 17:23:05 +0900

    feat: styleを少し修正

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -23,34 +23,36 @@ const Login: FC = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={style['login-page']}>
-      <main className={style['main-content']}>
-        <div className={style['field-content']}>
-          <label className={style['content']}>
-            <input
-              {...register('email', LOGIN_VALUES_OPTION.email)}
-              className={style['field']}
-              placeholder="Email"
-            />
-            {!!errors.email?.message && (
-              <p className={style['message']}>{errors.email.message}</p>
-            )}
-          </label>
-          <label className={style['content']}>
-            <input
-              {...register('password', LOGIN_VALUES_OPTION.password)}
-              className={style['field']}
-              placeholder="Password"
-            />
-            {!!errors.password?.message && (
-              <p className={style['message']}>{errors.password.message}</p>
-            )}
-          </label>
-        </div>
-        <button disabled={!isValid} className={style['action']}>
-          Login
-        </button>
-      </main>
-      <p className={style['message']}>forgot password?</p>
+      <div className={style['content']}>
+        <main className={style['main-content']}>
+          <div className={style['field-content']}>
+            <label className={style['content']}>
+              <input
+                {...register('email', LOGIN_VALUES_OPTION.email)}
+                className={style['field']}
+                placeholder="Email"
+              />
+              {!!errors.email?.message && (
+                <p className={style['message']}>{errors.email.message}</p>
+              )}
+            </label>
+            <label className={style['content']}>
+              <input
+                {...register('password', LOGIN_VALUES_OPTION.password)}
+                className={style['field']}
+                placeholder="Password"
+              />
+              {!!errors.password?.message && (
+                <p className={style['message']}>{errors.password.message}</p>
+              )}
+            </label>
+          </div>
+          <button disabled={!isValid} className={style['action']}>
+            Login
+          </button>
+        </main>
+        <p className={style['message']}>forgot password?</p>
+      </div>
     </form>
   );
 };
