751515fca78a7fac44ee015aefae9c353cd51a8c
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-03-20 14:55:02 +0900

    feat: バリデーションを追加

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,14 +1,42 @@
 import { FC } from 'react';
+import { useForm } from 'react-hook-form';
 
 import style from './Login.module.scss';
 
+import { LoginValues } from '@/models/validation/login';
+
 const Login: FC = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm<LoginValues>({ mode: 'onChange' });
+
+  console.log('errors  :  ', errors);
+
   return (
     <div className={style['login-page']}>
       <main className={style['main-content']}>
         <div className={style['field-content']}>
-          <input className={style['field']} placeholder="Email" />
-          <input className={style['field']} placeholder="Password" />
+          <label className={style['content']}>
+            <input
+              {...register('email', { required: true })}
+              className={style['field']}
+              placeholder="Email"
+            />
+            {!!errors.email && (
+              <p className={style['message']}>Emailを入力してください</p>
+            )}
+          </label>
+          <label className={style['content']}>
+            <input
+              {...register('password', { required: true })}
+              className={style['field']}
+              placeholder="Password"
+            />
+            {!!errors.password && (
+              <p className={style['message']}>パスワードを入力してください</p>
+            )}
+          </label>
         </div>
         <button className={style['action']}>Login</button>
       </main>
