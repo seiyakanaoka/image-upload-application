0ce260bf1798431090945188781a446d81d636b6
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-03-21 23:59:40 +0900

    feat: route追加

--- src/pages/routes/Routes.tsx
+++ src/pages/routes/Routes.tsx
@@ -7,12 +7,14 @@ import {
 import Page404 from '@/pages/error/404/404';
 import Home from '@/pages/home/Home';
 import Login from '@/pages/login/Login';
+import User from '@/pages/user/User';
 
 const Routes: FC = () => {
   return (
     <ReactRoutes>
       <ReactRouteDom path="/" element={<Home />} />
       <ReactRouteDom path="/login" element={<Login />} />;
+      <ReactRouteDom path="/user" element={<User />} />;
       <ReactRouteDom path="*" element={<Page404 />} />
     </ReactRoutes>
   );
