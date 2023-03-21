631512b3d745d77ecd56904b73e2b8980ce86295
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-03-21 23:56:15 +0900

    feat: homeに戻るバグ解消

---
+++ src/pages/user/User.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import style from './User.module.scss';
+
+const User: FC = () => {
+  return (
+    <div className={style['user-page']}>
+      <div></div>
+    </div>
+  );
+};
+
+export default User;
