742836ae1d2c209fa06da7c4eb27341637ff8e57
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-04-02 14:43:31 +0900

    feat: api連携完了

--- src/api/image/image.ts
+++ src/api/image/image.ts
@@ -2,11 +2,19 @@ import { ApiPath } from '@/constants/api';
 import { ImageResponse } from '@/models/api/image';
 import { axiosClient } from '@/utils/axiosClient';
 
-export const getImages = async (path: ApiPath): Promise<ImageResponse> => {
-  const response = await axiosClient.get(path, {
-    params: {
-      objectKey: 'fish.jpeg',
+export const getImages = async (
+  path: ApiPath
+): Promise<Array<ImageResponse>> => {
+  const response = await axiosClient.get(`${path}/user/${1}/images`);
+  return response.data;
+};
+
+export const postImage = async (path: ApiPath, image: Blob): Promise<void> => {
+  const formData = new FormData();
+  formData.append('image', image);
+  await axiosClient.post(path, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
     },
   });
-  return response.data;
 };
--- src/constants/api/image.ts
+++ src/constants/api/image.ts
@@ -2,6 +2,7 @@ export const apiPathImage = '/aws/s3/image';
 
 export const API_PATH_IMAGE = {
   image: '/aws/s3/image',
+  images: '/aws/s3',
 } as const;
 
 export type ApiPathImage = (typeof API_PATH_IMAGE)[keyof typeof API_PATH_IMAGE];
